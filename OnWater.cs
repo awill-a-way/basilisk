32c177cc49696532eff2a6bd88cfe84b52f2aff0
awill-a-way <aidantor03@gmail.com>
2026-02-02 11:34:25 -0600

    Refactor OnWater to use WaterSurface for height
    Refactor OnWater class to use WaterSurface for height sampling and update position accordingly.

--- OnWater.cs
+++ OnWater.cs
@@ -2,58 +2,24 @@ using UnityEngine;
 
 public class OnWater : MonoBehaviour
 {
-    private Waves currentWater;
-    private CharacterController controller;
+    public WaterSurface water;
+    public float onWaterRange = 0.1f;
+    public bool isOnWater = false;
 
-    public float gravity = -30f;
-    public float surfaceOffset = 0.05f;
-
-    private float verticalVelocity;
-
-    void Awake()
-    {
-        controller = GetComponent<CharacterController>();
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.TryGetComponent(out Waves water))
-        {
-            currentWater = water;
-        }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (other.TryGetComponent(out Waves water))
-        {
-            if (currentWater == water)
-                currentWater = null;
-        }
-    }
-
-    void Update()
+    void LateUpdate()
     {
         Vector3 pos = transform.position;
 
-        if (currentWater != null)
-        {
-            float waveHeight = currentWater.GetHeight(pos);
+        float waterY = water.SampleHeight(pos);
+        
+        if (pos.y < waterY)
+            pos.y = waterY;
 
-            if (pos.y <= waveHeight + 0.2f)
-            {
-                float snap = (waveHeight + surfaceOffset) - pos.y;
+        transform.position = pos;
 
-                if (snap > 0f)
-                    controller.Move(Vector3.up * snap);
-
-                verticalVelocity = -2f;
-                return;
-            }
-        }
-	
-        // Gravity when not on water
-        verticalVelocity += gravity * Time.deltaTime;
-        controller.Move(Vector3.up * verticalVelocity * Time.deltaTime);
+        if (pos.y < waterY + onWaterRange)
+            isOnWater = true;
+        else
+            isOnWater = false;
     }
 }
